51e7b20f488f20c8e467060437b36afae327c2c7
Niklas Gögge <n.goeggi@gmail.com>
2018-10-31 11:24:23 +0100

    Add output file flag & Improve resizing dimensions

--- main.go
+++ main.go
@@ -12,29 +12,34 @@ import (
 func main() {
 	var file string
 	var blurFactor float64
+	var outFile string
 
 	flag.StringVar(&file, "f", "/tmp/screenshot.png", "specify the image you want to blur")
 	flag.Float64Var(&blurFactor, "b", 1.5, "specify the blur factor")
+	flag.StringVar(&outFile, "o", "/tmp/screenshot.png", "specify output file")
+	flag.Parse()
 
 	start := time.Now()
-	blur(file, blurFactor)
+	blur(file, outFile, blurFactor)
 	elapsed := time.Since(start)
 	fmt.Printf("Elapsed time: %s\n", elapsed)
 }
 
-func blur(file string, blurFactor float64) {
+func blur(file string, outFile string, blurFactor float64) {
 	src, err := imaging.Open(file)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to open image: %v", err)
+		fmt.Fprintf(os.Stderr, "failed to open image: %v\n", err)
+		return
 	}
 
-	src = imaging.Resize(src, 300, 300, imaging.Lanczos)
+	width := src.Bounds().Dx()
+	height := src.Bounds().Dy()
 
+	src = imaging.Resize(src, int(width/10), int(height/10), imaging.Lanczos)
 	src = imaging.Blur(src, blurFactor)
+	src = imaging.Resize(src, width, height, imaging.Lanczos)
 
-	src = imaging.Resize(src, 1920, 1080, imaging.Lanczos)
-
-	err = imaging.Save(src, file)
+	err = imaging.Save(src, outFile)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to save image: %v", err)
 	}
